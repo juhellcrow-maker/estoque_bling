db5a73292e06c923d1adbde881857727ff40aede
juhellcrow-maker <ju.hellcrow@gmail.com>
2026-04-26 18:00:08 -0300

    Implement buscar_estoque_fornecedor function
    Add a function to simulate Procortex stock return.

--- procortex.py
+++ procortex.py
@@ -1,1 +1,10 @@
+# procortex.py
 
+def buscar_estoque_fornecedor():
+    """
+    Simula o retorno do Procortex.
+    Depois você pluga o Metabase real.
+    """
+    return {
+        "4504400": 162
+    }
