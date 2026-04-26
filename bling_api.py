7ad0c32541afe9bc282caf3416bc00870b32186d
juhellcrow-maker <ju.hellcrow@gmail.com>
2026-04-26 17:59:29 -0300

    Add function to simulate unpaid orders from Bling
    This function simulates unpaid orders from Bling, returning a list of orders with item codes and quantities.

--- bling_api.py
+++ bling_api.py
@@ -1,1 +1,45 @@
+# bling.py
 
+def buscar_pedidos_nao_faturados():
+    """
+    Simula pedidos não faturados do Bling.
+    Cada pedido contém itens com código e quantidade.
+    """
+    return [
+        {
+            "id": 1,
+            "itens": [
+                {"codigo": "4504400", "quantidade": 2}
+            ]
+        },
+        {
+            "id": 2,
+            "itens": [
+                {"codigo": "4504400", "quantidade": 1}
+            ]
+        },
+        {
+            "id": 3,
+            "itens": [
+                {"codigo": "4504400", "quantidade": 2}
+            ]
+        },
+        {
+            "id": 4,
+            "itens": [
+                {"codigo": "4504400", "quantidade": 1}
+            ]
+        },
+        {
+            "id": 5,
+            "itens": [
+                {"codigo": "4504400", "quantidade": 1}
+            ]
+        },
+        {
+            "id": 6,
+            "itens": [
+                {"codigo": "4504400", "quantidade": 1}
+            ]
+        }
+    ]
